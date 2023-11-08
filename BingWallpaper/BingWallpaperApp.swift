72c417e2ff9a794402b5afaff4fb1c5e347ab842
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-11-08 12:04:10 -0400

    Initial Commit

---
+++ BingWallpaper/BingWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingWallpaperApp.swift
+//  BingWallpaper
+//
+//  Created by Allister Isaiah Harvey on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BingWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
